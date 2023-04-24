247ae22d08aeb334997bc6aa7f16e6561505824c
Matt Powell <matt.powell@usabilityhub.com>
2023-04-24 14:18:28 +1200

    add radio and checkbox components

---
+++ app/frontend/atoms/Checkbox/Checkbox.tsx
@@ -0,0 +1,44 @@
+import React, { forwardRef, useEffect, useRef } from 'react';
+import { mergeRefs } from 'react-merge-refs';
+import clsx from 'clsx';
+
+import Icon from '../Icon';
+import Radio from '../Radio';
+
+import { CheckboxProps } from './Checkbox.types';
+
+import './Checkbox.css';
+
+const CheckboxIcon = () => (
+  <Icon>
+    <rect x={3} y={3} width={18} height={18} rx={2} />
+    <path d="m7.5 12 3 3 6-6" pathLength={1} />
+  </Icon>
+);
+
+const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ className, indeterminate, children = <CheckboxIcon />, ...props }, ref) => {
+    const ownRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+      if (ownRef.current && indeterminate !== undefined) {
+        ownRef.current.indeterminate = indeterminate;
+      }
+    }, [indeterminate]);
+
+    return (
+      <Radio
+        ref={mergeRefs([ref, ownRef])}
+        type="checkbox"
+        className={clsx('checkbox', className)}
+        {...props}
+      >
+        {children}
+      </Radio>
+    );
+  }
+);
+
+Checkbox.displayName = 'Checkbox';
+
+export default Checkbox;
---
+++ app/frontend/atoms/Radio/Radio.typetest.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* c8 ignore file */
+
+import { RadioProps } from './Radio.types';
+
+/**
+ * Describe: RadioProps
+ */
+
+// @ts-expect-no-error - No required props
+const testEmptyProps: RadioProps = {};
