d573715287830b98e326ff1c47713a9bbf2b3363
Matt Powell <matt.powell@usabilityhub.com>
2023-07-03 12:42:09 +1200

    require login for admin

--- app/frontend/pages/Admin/index.tsx
+++ app/frontend/pages/Admin/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { BreadcrumbProvider } from '@/molecules/Breadcrumbs';
+import { useAuthentication } from '@/organisms/Authentication';
 import Header from '@/organisms/Header';
 
 import CurrentFestivalRedirect from './CurrentFestivalRedirect';
@@ -10,6 +11,12 @@ import Festival from './Festival';
 import './Admin.css';
 
 const Admin: React.FC = () => {
+  const { loading, user } = useAuthentication();
+
+  if (!loading && !user?.permissions.length) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <BreadcrumbProvider label="Admin" path="admin">
       <div>
