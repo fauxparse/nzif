232ada698d3685a81db6c1fedf5fd2d06c61607b
Matt Powell <matt.powell@usabilityhub.com>
2023-08-07 14:05:23 +1200

    email niceties

---
+++ app/helpers/email_helper.rb
@@ -0,0 +1,6 @@
+module EmailHelper
+  def email_image_tag(image, **options)
+    attachments[image] = Rails.root.join("app/assets/images/#{image}").read
+    image_tag attachments[image].url, **options
+  end
+end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
+  helper EmailHelper
+
   default from: 'matt@improvfest.nz'
   layout 'mailer'
 end
