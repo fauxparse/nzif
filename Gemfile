7a16c978f19fd1a4d57ad0326b418645f5bc331e
Matt Powell <matt.powell@usabilityhub.com>
2023-08-22 10:14:00 +1200

    add raygun

--- Gemfile
+++ Gemfile
@@ -87,6 +87,8 @@ gem 'icalendar', '~> 2.9'
 
 gem 'rest-client'
 
+gem 'raygun4ruby'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
--- app/frontend/App.tsx
+++ app/frontend/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import { PortalManager } from '@chakra-ui/portal';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import rg4js from 'raygun4js';
 
 import PlacenameProvider from './atoms/Placename/PlacenameProvider';
 import AuthenticationProvider from './organisms/Authentication/AuthenticationProvider';
 import Routes from './Routes';
 
 const queryClient = new QueryClient();
+
+rg4js('apiKey', '506C6CapQPSWt8QaeAfw');
+rg4js('enableCrashReporting', import.meta.env.PROD);
 
 const App: React.FC = () => (
   <QueryClientProvider client={queryClient}>
