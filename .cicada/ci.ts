16754165cfce2b9fbe2b7de1c107f30c78894588
Matt Powell <matt.powell@usabilityhub.com>
2023-08-24 22:30:38 +1200

    set up cicada

---
+++ .cicada/ci.ts
@@ -0,0 +1,29 @@
+import { Job, Pipeline } from "https://deno.land/x/cicada@v0.1.50/mod.ts";
+
+const job = new Job({
+  name: "CI",
+  image: "node:18",
+  steps: [
+    cacheDirectories: ["node_modules"],
+    steps: [
+      {
+        name: "Install dependencies",
+        run: "yarn install",
+      },
+      {
+        name: "Run tests",
+        run: "yarn test",
+      },
+    ],
+  ],
+});
+
+export default new Pipeline(
+  [job],
+  {
+    on: {
+      pullRequest: ["main"],
+      push: ["main"],
+    },
+  },
+);
