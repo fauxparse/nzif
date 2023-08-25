522179e44a29c0d8f1295736d71721b493b32e79
Matt Powell <matt.powell@usabilityhub.com>
2023-08-26 10:24:07 +1200

    ci

--- .cicada/ci.ts
+++ .cicada/ci.ts
@@ -3,18 +3,16 @@ import { Job, Pipeline } from "https://deno.land/x/cicada@v0.1.50/mod.ts";
 const job = new Job({
   name: "CI",
   image: "node:18",
+  cacheDirectories: ["node_modules"],
   steps: [
-    cacheDirectories: ["node_modules"],
-    steps: [
-      {
-        name: "Install dependencies",
-        run: "yarn install",
-      },
-      {
-        name: "Run tests",
-        run: "yarn test",
-      },
-    ],
+    {
+      name: "Install dependencies",
+      run: "yarn install",
+    },
+    {
+      name: "Run tests",
+      run: "yarn test",
+    },
   ],
 });
 
