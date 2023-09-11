71134df4d143969fc037ea96903792f5540aeac5
Matt Powell <matt.powell@usabilityhub.com>
2023-09-11 12:26:14 +1200

    use correct stripe key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,4 +3,4 @@ Rails.configuration.stripe = {
   secret_key: ENV.fetch('STRIPE_SECRET_KEY', nil),
 }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = ENV.fetch('STRIPE_SECRET_KEY', nil)
