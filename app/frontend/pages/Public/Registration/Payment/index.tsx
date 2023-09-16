366e3b0f2e940b6c6c8f4423d876e82162b34289
Matt Powell <matt.powell@usabilityhub.com>
2023-09-16 12:56:02 +1200

    redirect to registration login

--- app/frontend/pages/Public/Registration/Payment/index.tsx
+++ app/frontend/pages/Public/Registration/Payment/index.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { useRegistrationContext } from '../RegistrationContext';
 import { RegistrationPhase } from '@/graphql/types';
 
@@ -7,7 +10,15 @@ import General from './General';
 import './Payment.css';
 
 export const Component: React.FC = () => {
-  const { loading, festival } = useRegistrationContext();
+  const { loading, festival, registration } = useRegistrationContext();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loading && !registration?.id) {
+      navigate(festival ? `/${festival.id}/register` : '/');
+    }
+  }, [loading, registration, festival, navigate]);
 
   if (loading) return null;
 
