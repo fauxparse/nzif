7d56175edff92f8eb075dc9fa6ec6076202a5e3d
Matt Powell <matt.powell@usabilityhub.com>
2023-10-06 13:36:08 +1300

    fix session mailer from address

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -6,7 +6,7 @@ class SessionMailer < ApplicationMailer
       to: 'registrations@improvfest.nz',
       bcc: recipients.map { |r| email_address_with_name(r.email, r.name) },
       subject: "NZIF: #{message.subject}",
-      from: email_address_with_name(message.sender.email, message.sender.name),
+      reply_to: email_address_with_name(message.sender.email, message.sender.name),
     )
   end
 end
