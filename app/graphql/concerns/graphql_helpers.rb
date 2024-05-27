b2cfaac36a0849e9794a7255bad394b49164de73
Matt Powell <matt.powell@usabilityhub.com>
2024-05-28 09:46:49 +1200

    fix current_festival

--- app/graphql/concerns/graphql_helpers.rb
+++ app/graphql/concerns/graphql_helpers.rb
@@ -2,7 +2,8 @@ module GraphqlHelpers
   extend ActiveSupport::Concern
 
   def current_festival
-    context[:current_festival] ||= Festival.upcoming.first
+    # context[:current_festival] ||= Festival.upcoming.first
+    context[:current_festival] ||= Festival.order(starts_at: :desc).first
   end
 
   def current_registration
