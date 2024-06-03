bb3b5c45f954cb47574dfe712e835d3bf7fbc7da
Matt Powell <matt.powell@usabilityhub.com>
2024-06-03 20:59:51 +1200

    add presenters to activities

--- app/frontend/components/pages/admin/ActivityEditor/Edit.tsx
+++ app/frontend/components/pages/admin/ActivityEditor/Edit.tsx
@@ -1,11 +1,12 @@
+import { PersonPicker } from '@/components/molecules/PersonPicker';
 import { Editor } from '@/components/organisms/Editor';
-import { ActivityAttributes } from '@/graphql/types';
+import { ActivityAttributes, ActivityType } from '@/graphql/types';
 import { useMutation } from '@apollo/client';
 import { notifications } from '@mantine/notifications';
 import { createFormFactory } from '@tanstack/react-form';
 import { pick } from 'lodash-es';
 import { UpdateActivityMutation } from './queries';
-import { Activity, ActivityDetails } from './types';
+import { Activity, ActivityDetails, isShow, isWorkshop } from './types';
 
 type EditProps = {
   activity: Activity;
@@ -14,21 +15,48 @@ type EditProps = {
 const formFactory = createFormFactory<ActivityDetails>({
   defaultValues: {
     name: '',
+    type: ActivityType.Workshop,
     slug: '',
     description: '',
+    presenters: [],
   },
 });
+
+const getDefaultValuesFromActivity = (activity: Activity): ActivityDetails => {
+  const result = pick(activity, ['name', 'type', 'slug', 'description']) as ActivityDetails;
+
+  if (isWorkshop(activity)) {
+    result.presenters = activity.tutors.map((presenter) => ({
+      id: presenter.id,
+      name: presenter.name,
+    }));
+  } else if (isShow(activity)) {
+    result.presenters = activity.directors.map((presenter) => ({
+      id: presenter.id,
+      name: presenter.name,
+    }));
+  }
+  return result;
+};
 
 export const Edit: React.FC<EditProps> = ({ activity }) => {
   const [updateMutation] = useMutation(UpdateActivityMutation);
 
+  const hasPresenters = isWorkshop(activity) || isShow(activity);
+
   const form = formFactory.useForm({
-    defaultValues: activity,
+    defaultValues: getDefaultValuesFromActivity(activity),
     onSubmit: ({ value }) => {
+      const attributes = pick(value, ['description']) as ActivityAttributes;
+
+      if (hasPresenters) {
+        attributes.profileIds = value.presenters.map((presenter) => presenter.id);
+      }
+
       updateMutation({
         variables: {
           id: activity.id,
-          attributes: pick(value, ['description']) as ActivityAttributes,
+          attributes,
         },
         update: (cache, { data }) => {
           if (!data?.updateActivity?.activity) return;
@@ -50,7 +78,20 @@ export const Edit: React.FC<EditProps> = ({ activity }) => {
 
   return (
     <div className="activity-editor__edit">
-      <h1>{activity.name}</h1>
+      {hasPresenters && (
+        <form.Field name="presenters">
+          {(field) => (
+            <PersonPicker
+              value={field.state.value}
+              allowAdd
+              onChange={(value) => {
+                field.handleChange(value);
+                form.handleSubmit();
+              }}
+            />
+          )}
+        </form.Field>
+      )}
       <form.Field name="description">
         {(field) => (
           <Editor
