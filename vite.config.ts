8a83b46437463777e1801b0f1e4113c933e7ed62
Matt Powell <matt.powell@usabilityhub.com>
2024-06-21 15:54:15 +1200

    switch to vite with swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import * as path from 'node:path';
 import yaml from '@modyfi/vite-plugin-yaml';
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import ruby from 'vite-plugin-ruby';
 import { configDefaults, defineConfig } from 'vitest/config';
@@ -33,10 +33,9 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: 'tests/setup.ts',
     coverage: {
-      provider: 'c8',
+      provider: 'istanbul',
       exclude: [...(configDefaults.coverage.exclude || []), '**/tests/setup.ts'],
     },
-    threads: false,
   },
   define: {
     STRIPE_PUBLIC_KEY: '"process.env.STRIPE_PUBLISHABLE_KEY"',
