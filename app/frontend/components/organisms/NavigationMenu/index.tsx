e3bfdb0978129a8eb9c9d046613627fdfb7cb71b
Matt Powell <matt.powell@usabilityhub.com>
2024-06-27 08:58:16 +1200

    navigation bar theming

--- app/frontend/components/organisms/NavigationMenu/index.tsx
+++ app/frontend/components/organisms/NavigationMenu/index.tsx
@@ -1,23 +1,25 @@
-import Button from '@/components/atoms/Button';
 import Drawer from '@/components/molecules/Drawer';
 import MenuIcon from '@/icons/MenuIcon';
 import { useDisclosure } from '@mantine/hooks';
 
 import { AdminNavigation } from './AdminNavigation';
 import './NavigationMenu.css';
+import { ActionIcon } from '@mantine/core';
 
 const NavigationMenu: React.FC = () => {
   const [opened, { open, close }] = useDisclosure();
 
   return (
     <>
-      <Button
+      <ActionIcon
         className="button--menu"
-        variant="ghost"
-        leftSection={<MenuIcon />}
+        variant="subtle"
+        data-color="neutral"
         aria-expanded={opened}
         onClick={open}
-      />
+      >
+        <MenuIcon />
+      </ActionIcon>
       <Drawer opened={opened} title="Hello" onClose={close}>
         <p>Todo</p>
         <AdminNavigation />
