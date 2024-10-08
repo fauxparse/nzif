96d434f97e136986b2092f49d936550dc46f8edc
Matt Powell <matt.powell@usabilityhub.com>
2024-10-09 12:41:08 +1300

    don't promote from waitlist after session has started

--- app/interactors/waitlists/check.rb
+++ app/interactors/waitlists/check.rb
@@ -5,6 +5,8 @@ module Waitlists
     def call
       skip_authorization!
 
+      return if session.starts_at.past?
+
       while session.placements.count < session.capacity
         registration = session.waitlist.includes(:registration).first&.registration
         break unless registration
