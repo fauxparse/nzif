5c0bf9f604b75561b0488631f981f43bfc4be71d
Matt Powell <fauxparse@gmail.com>
2026-09-07 21:18:34 +1200

    require login for payment page

--- app/frontend/routes/register/payment.tsx
+++ app/frontend/routes/register/payment.tsx
@@ -1,6 +1,17 @@
 import { Payment } from '@/components/pages/Registration/Payment';
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/register/payment')({
   component: Payment,
+  beforeLoad: ({ location, context }) => {
+    if (context.auth.user === null) {
+      throw redirect({
+        to: '/login',
+        search: {
+          redirect: location.href,
+        },
+        replace: true,
+      });
+    }
+  },
 });
